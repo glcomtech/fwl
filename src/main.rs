a89024cbe0e5122ebad1ecf4c4d6ab714f110410
git-user-cpp <akm.newman.mail@gmail.com>
2025-06-16 12:17:39 +0300

    project structure improved

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-pub mod restore_fun;
-use restore_fun::{print_license_info, validate_root_priviliges, restore_ipt_rules, display_ipt_rules};
+use fwl::default_iptables_restore;
 
 fn main() {
-    print_license_info();
-    validate_root_priviliges(); // exits if not root
-    restore_ipt_rules();
-    display_ipt_rules();
+    default_iptables_restore();
 }
--- src/restore_functionality/restore_fun.rs
+++ src/restore_functionality/restore_fun.rs
@@ -1,23 +1,6 @@
-use std::process::Command;
-use std::fs::File;
 use colored::Colorize;
-
-/// Prints license info
-pub fn print_license_info() {
-    println!("fwl  Copyright (C) 2025  Andrew Kushyk\n\
-This program comes with ABSOLUTELY NO WARRANTY; for details see https://www.gnu.org/licenses/gpl-3.0.html/\n\
-This is free software, and you are welcome to redistribute it\n\
-under certain conditions; for details see https://www.gnu.org/licenses/gpl-3.0.html/\n");
-}
-
-/// validates if user has root priviliges. Terminates the program otherwise.
-pub fn validate_root_priviliges() {
-    if unsafe { libc::getuid() } != 0 {
-        eprintln!("{}", "This program requires root privileges. Please run with sudo.\nExample: sudo ./fwl".red());
-        // exits if not root
-        std::process::exit(1);
-    }
-}
+use std::fs::File;
+use std::process::Command;
 
 /// restores iptables firewall rules from /etc/iptables/iptables.rules file
 pub fn restore_ipt_rules() {
