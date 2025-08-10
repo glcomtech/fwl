14553e9cc2b15667459c728c90de2a79760c48e3
Andrii Kushyk <akm.newman.mail@gmail.com>
2025-08-10 19:35:28 +0300

    documentation was updated

--- docs/src-files.js
+++ docs/src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["fwl",["",[["helper_functionality",[],["helper_fun.rs"]],["restore_functionality",[],["restore_fun.rs"]]],["helper_functionality.rs","lib.rs","restore_functionality.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[172]}
+createSrcSidebar('[["fwl",["",[["helper_functionality",[],["helper_fun.rs"]],["restore_functionality",[],["restore_fun.rs"]]],["helper_functionality.rs","lib.rs","restore_functionality.rs"]]]]');
+//{"start":19,"fragment_lengths":[172]}
